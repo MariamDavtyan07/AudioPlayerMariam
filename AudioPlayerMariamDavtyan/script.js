227b77f7b685b03b694fe709f3dc2f47b92b51e7
MariamDavtyan07 <mariamdavtyan478@gmail.com>
2024-05-13 20:46:30 +0400

    changes

--- AudioPlayerMariamDavtyan/script.js
+++ AudioPlayerMariamDavtyan/script.js
@@ -1,21 +1,27 @@
 let data = {
-    title: ["Shape of my heart",
-            "Ov khandipi",
-            "If you go",
-            "Last chance to love",
-             "Daisy"],
+    title: [
+        "Shape of my heart",
+        "Ov khandipi",
+        "If you go",
+        "Last chance to love",
+        "Daisy",
+        "Mon Amour"],
     song: [
-    "songs/Sting - Shape Of My Heart (11).mp3",
-    "songs/Nemra_-_Last_chance_to_love_76972100.mp3",
-    "songs/Nemra_-_Last_chance_to_love_76972100.mp3",
-    "songs/2e849b6a74592e9a96506acec6b3587c_nemra-daisy.mp3",
-    "songs/PROJECT LA feat. Gor Sujyan - Ov Khandipi (Eghishe Charents) 2018.mp3"],
-    poster: ["https://upload.wikimedia.org/wikipedia/it/a/a2/Shape_of_My_Heart_%28Sting%29.png",
-             "https://pbs.twimg.com/ext_tw_video_thumb/1437501294315454476/pu/img/quqaBKfO8mqVjEV2.jpg:large",
-            "https://i1.sndcdn.com/artworks-000133937425-n6zixi-t500x500.jpg",
-             "https://i.ytimg.com/vi/8Gy2ZDGLteQ/sddefault.jpg"]
+        "songs/Sting - Shape Of My Heart (11).mp3",
+        "songs/PROJECT LA feat. Gor Sujyan - Ov Khandipi (Eghishe Charents) 2018.mp3", ,
+        "Nemra - If you go (www.mp3erger.ru) 2023.mp3",
+        "songs/Nemra_-_Last_chance_to_love_76972100.mp3",
+        "songs/2e849b6a74592e9a96506acec6b3587c_nemra-daisy.mp3",
+        "songs/Slimane - Mon Amour (1).mp3"],
+    poster: [
+        "https://upload.wikimedia.org/wikipedia/it/a/a2/Shape_of_My_Heart_%28Sting%29.png",
+        "https://i.ytimg.com/vi/lYx1A2cDus0/maxresdefault.jpg",
+        "https://i.ytimg.com/vi/8Gy2ZDGLteQ/sddefault.jpg",
+        "https://i.ytimg.com/vi/m9eK8vtNtS0/sddefault.jpg",
+        "https://i.ytimg.com/vi/iIdCJUBb0_Q/maxresdefault.jpg",
+        "https://upload.wikimedia.org/wikipedia/it/0/08/Slimane_-_Mon_Amour.png"
+    ],
 }
-
 
 let song = new Audio()
 window.onload = function(){
