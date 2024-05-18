838d1cadae4cc60a955a00392ae5f0ba86940f96
Alen-Arustamyan <alen.arustamyan@tumo.org>
2024-05-18 13:14:36 +0400

    changes

--- AudioPlayerMariamDavtyan/script.js
+++ AudioPlayerMariamDavtyan/script.js
@@ -2,24 +2,23 @@ let data = {
     title: [
         "Shape of my heart",
         "Ov khandipi",
-        "If you go",
         "Last chance to love",
-        "Daisy",
-        "Mon Amour"],
+        "If you go",
+        "Daisy",],
     song: [
         "songs/Sting - Shape Of My Heart (11).mp3",
-        "songs/PROJECT LA feat. Gor Sujyan - Ov Khandipi (Eghishe Charents) 2018.mp3", ,
-        "Nemra - If you go (www.mp3erger.ru) 2023.mp3",
+        "songs/PROJECT LA feat. Gor Sujyan - Ov Khandipi (Eghishe Charents) 2018.mp3", 
         "songs/Nemra_-_Last_chance_to_love_76972100.mp3",
+        "songs/Nemra-If_you_go.mp3",
         "songs/2e849b6a74592e9a96506acec6b3587c_nemra-daisy.mp3",
-        "songs/Slimane - Mon Amour (1).mp3"],
+    ],
     poster: [
         "https://upload.wikimedia.org/wikipedia/it/a/a2/Shape_of_My_Heart_%28Sting%29.png",
         "https://i.ytimg.com/vi/lYx1A2cDus0/maxresdefault.jpg",
-        "https://i.ytimg.com/vi/8Gy2ZDGLteQ/sddefault.jpg",
         "https://i.ytimg.com/vi/m9eK8vtNtS0/sddefault.jpg",
+        "https://i.ytimg.com/vi/8Gy2ZDGLteQ/sddefault.jpg",
         "https://i.ytimg.com/vi/iIdCJUBb0_Q/maxresdefault.jpg",
-        "https://upload.wikimedia.org/wikipedia/it/0/08/Slimane_-_Mon_Amour.png"
+
     ],
 }
 
@@ -56,5 +55,77 @@ song.addEventListener("timeupdate",function(){
     let position = song.currentTime/song.duration
     fill[0].style.marginLeft = position * 99 + "%"
 
+    convertTime(song.currentTime)
+    if(song.ended){
+        next()
+    }
     
 })
+
+function convertTime(seconds){
+    let currentTime = document.getElementsByClassName("currentTime")
+    let min =   Math.floor(seconds/60)
+    let sec =   Math.floor(seconds%60)
+    min = (min<10) ? "0" + min : min
+    sec = (sec<10) ? "0" + sec : sec
+    currentTime[0].textContent = min + ":" + sec
+    totalTime(Math.round(song.duration))
+
+
+
+
+
+}
+function totalTime(seconds){
+    let currentTime = document.getElementsByClassName("currentTime")
+    let min =   Math.floor(seconds/60)
+    let sec =   Math.floor(seconds%60)
+    min = (min<10) ? "0" + min : min
+    sec = (sec<10) ? "0" + sec : sec
+    currentTime[0].textContent+= " / " + min + ":" + sec
+}
+function prev(){
+    currentSong--
+    let play = document.getElementById("play")
+    if(currentSong < 0){
+        currentSong = data.song.length-1
+    }
+    
+    playSong()
+    play.src = "images/pause.png"
+}
+function next(){
+    currentSong++
+    let play = document.getElementById("play")
+    if(currentSong == data.song.length){
+        currentSong = 0
+    }
+    
+    playSong()
+    play.src = "images/pause.png"
+}
+
+function mute(){
+    let mute = document.getElementById("mute")
+    if(song.muted){
+        song.muted = false
+        mute.src = "images/volume.png"
+
+    }
+    else{
+        song.muted = true
+        mute.src = "images/volume-mute.png"
+    }
+}
+function decrease(){
+    song.volume -= 0.2
+    if(song.volume <=0.1){
+        mute.src = "images/volume-mute.png"
+    }
+}
+function increase(){
+    song.volume += 0.2
+    if(song.volume >=0.2){
+        mute.src = "images/volume.png"
+    }
+}
