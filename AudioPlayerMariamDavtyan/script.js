9f306facd5608babfdad3691ae3a1f3529e22af3
MariamDavtyan07 <mariamdavtyan478@gmail.com>
2024-05-20 19:10:24 +0400

    changes

--- AudioPlayerMariamDavtyan/script.js
+++ AudioPlayerMariamDavtyan/script.js
@@ -129,3 +129,25 @@ function increase(){
         mute.src = "images/volume.png"
     }
 }
+function changeSong(index) {
+    currentSong = index;
+    playSong();
+}
+
+function toggleDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
